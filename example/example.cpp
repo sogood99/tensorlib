cd37ff3a771c50ae99997726ba669413014f1b27
Onii Chan <46487694+sogood99@users.noreply.github.com>
2024-11-25 21:22:07 -0500

    Add transpose multiply to cpu_handler

--- example/example.cpp
+++ example/example.cpp
@@ -8,10 +8,13 @@ int main() {
   variable x = TensorFactory::create(
       vector<float>{1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f,
                     11.0f, 12.0f},
-      vector<size_t>{2, 2, 3}, Device::GPU, true);
+      vector<size_t>{2, 2, 3}, Device::CPU, true);
 
-  variable y =
-      TensorFactory::randn(vector<size_t>{2, 3, 4}, 0., 1., Device::GPU, true);
+  variable y = TensorFactory::create(
+      vector<float>{2.f,  3.f,  4.f,  5.f,  6.f,  7.f,  8.f,  9.f,
+                    10.f, 11.f, 12.f, 13.f, 14.f, 15.f, 16.f, 17.f,
+                    18.f, 19.f, 20.f, 21.f, 22.f, 23.f, 24.f, -2.f},
+      vector<size_t>{2, 3, 4}, Device::CPU, true);
 
   cout << "x: " << x->to_string() << endl;
   cout << "y: " << y->to_string() << endl;
@@ -27,7 +30,7 @@ int main() {
   variable init_grad_w = make_shared<Tensor>(
       vector<float>{1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f,
                     11.0f, 12.0f, 13.0f, 14.0f, 15.0f, 16.0f},
-      vector<size_t>{2, 2, 4}, Device::GPU);
+      vector<size_t>{2, 2, 4}, Device::CPU);
 
   l->backward(init_grad_w);
 
