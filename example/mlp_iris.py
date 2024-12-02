Fix subtract gradient bug

example/mlp_iris.py
@@ -9,7 +9,7 @@ input_size = 4  # There are 4 features in the Iris dataset
 hidden_size = 5
 output_size = 1
 learning_rate = 0.01
-epochs = 2
+epochs = 100
 
 # Load Iris dataset
 iris = load_iris()
@@ -17,43 +17,49 @@ X_data = iris.data
 y_data = iris.target
 
 # Convert to binary classification: class 0 (Setosa) vs class 1 (Versicolor)
-binary_classes = np.where(y_data < 2, 1, 0)  
+binary_classes = np.where(y_data < 2, 1, 0)
 
-X_train, X_test, y_train, y_test = train_test_split(X_data, binary_classes, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X_data, binary_classes, test_size=0.2, random_state=42
+)
 
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
 X = tl.Tensor(X_train.astype(np.float32), device=tl.Device.CPU, requires_grad=False)
-y = tl.Tensor(y_train.astype(np.float32).reshape(-1, 1), device=tl.Device.CPU, requires_grad=False)
+y = tl.Tensor(
+    y_train.astype(np.float32).reshape(-1, 1), device=tl.Device.CPU, requires_grad=False
+)
 
 W1 = tl.randn([input_size, hidden_size], requires_grad=True)
 b1 = tl.zeros([hidden_size], requires_grad=True)
 W2 = tl.randn([hidden_size, output_size], requires_grad=True)
 b2 = tl.zeros([output_size], requires_grad=True)
 
+
 def sigmoid(x):
     return 1.0 / (1.0 + tl.exp(-x))
 
+
 def forward(X):
     hidden = tl.relu(X @ W1 + b1)
+    # print(hidden.shape)
     output = sigmoid(hidden @ W2 + b2)
+    # print(output.shape)
+    # print("Output:", output)
     return output
 
+
 def binary_cross_entropy(pred, target):
-    eps = 1e-7  # Avoid log(0)
-    return tl.mean(
-        -(target * tl.log(pred + eps) + (1 - target) * tl.log(1 - pred + eps))
-    )
+    return -tl.mean(target * tl.log(pred) + (1 - target) * tl.log(1 - pred))
+
 
 losses = []
 
 for epoch in range(epochs):
     y_pred = forward(X)
     loss = binary_cross_entropy(y_pred, y)
-
-    print(loss, y_pred)
 
     loss.backward()
 
@@ -64,10 +70,10 @@ for epoch in range(epochs):
     b2 -= learning_rate * b2.grad
 
     # Clear gradients
-    W1.zero_()
-    b1.zero_()
-    W2.zero_()
-    b2.zero_()
+    W1.grad.zero_()
+    b1.grad.zero_()
+    W2.grad.zero_()
+    b2.grad.zero_()
 
     losses.append(loss.item())
     print(f"Epoch {epoch}, Loss: {loss.item()}")